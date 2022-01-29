219c8e7c6838918c071e3f914d458d757268bf57
Reid Schroder <reidschroder2@gmail.com>
2022-01-28 22:37:10 -0500

    added all weather to current weather container

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,9 @@ searchBtn = document.querySelector("#search-btn")
 clearHistoryBtn = document.querySelector("#clear-history-btn")
 cityInput = document.querySelector("#city-input")
 citySearch = document.querySelector("#city-search")
+uvIndex = document.querySelector("#uv-index")
+searchList = document.querySelector("#search-list")
+forecastContainer = document.querySelector("#forecast-container")
 
 
 //currentDate.textContent = moment().format("MMM Do YY")
@@ -18,38 +21,152 @@ var formSubmitHandler = function(event) {
 
     if (city) {
         //get weather funtion
-        displayWeather(city);
+        fetchWeather(city);
         //forecast funtion
+        displayWeather(city);
+        displayForecast(city);
+        
     }
 
     else {
-        alert("Enter a valid city")
+        alert("Please Enter A Valid City")
     }
 
 }
 
 //display weather function 
 
-var displayWeather =function(city) {
+var fetchWeather =function(city) {
 
-var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+var weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey + "&units=imperial";
 
     fetch(weatherApi).then(function(response){
         if(response.ok){
         response.json().then(function(data){
             var todayDate = new Date(data.dt * 1000).toLocaleDateString();
             citySearch.textContent = city + " " + todayDate;
+
+            // clear historical data
+            fetchWeather.innerHTML = "";
+
+           
+
+            
+            
+            
+            // Lat & Lon 
+            let lat = data.coord.lat;
+            let lon = data.coord.lon;
+            let uvLink = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&APPID=" + apiKey + "&units=imperial";
+
+            fetch(uvLink).then(function(response){
+                response.json().then(function(data){
+
+                    console.log(data);
+
+
+                    displayWeather(data);
+                    displayForecast(data);
+
+            
+                })
+            })
         
         })}
     })
     
 }
 
-//display forecast function
+
+
+//display Current Weather
+
+var displayWeather = function(data) {
+    console.log(data);
+
+    
+
+    let weather = data.current;
+   console.log(weather);
+
+    let temp = weather.temp;
+    let humidity = weather.humidity;
+    let wind = weather.wind_gust;
+    let uvi = weather.uvi
+
+    console.log(temp, humidity, wind, uvi);
+
+
+    temperature = document.querySelector("#temperature");
+    temperature.innerHTML = "Current Temperature: " + temp + " \u00B0 F";
+
+    currentHumidity = document.querySelector("#humidity");
+    currentHumidity.innerHTML = "Humidity: " + humidity;
+
+    windGust = document.querySelector("#wind-gust");
+    windGust.innerHTML = "Wind: " + wind + " MPH";
+
+    uv = document.querySelector("#uv-index");
+    uv.innerHTML = "UV Index: " + uvi;
+
+    // display the uv index color chart
+    if (uv < 3) {
+        uv.classList.add("bg-success");
+
+    }
+
+    
+    
+}
+// Display 5 Day Forecast
+function displayForecast(data) {
+    console.log(data);
+
+    var forecastEl = document.createElement("div");
+    forecastEl.classList.add("weather-card");
+
+    
 
 
 
+   // var dailyWeather = weather.daily;
+
+    
+    
+}
+
+// ======= Local Storage ========== ???????????????
+
+var locationClickHandler = function(event) {
+    var city = event.target.textContent;
+    displayWeather(city);
+    displayForecast(city);
+}
+
+let storage = JSON.parse(localStorage.getItem("storage")) || {};
+
+// Store in local storage
+
+var saveStorage = function() {
+    var city = cityInput.value.trim();
+
+    if (storage.indexOf(city) == -1) {
+        storage.push(city);
+        localStorage.setItem("storage", JSON.stringify(storage));
+    }
+    searchList.innerHTML = "";
+
+    for (var i = 0; i < storage.length; i++) {
+        var location = storage[i];
+        var button = document.createElement("button");
+        button.textContent = location;
+        button.classList.add("btn");
+        searchList.appendChild(button);
+
+        button.addEventListener("click", locationClickHandler);
+    }
+}
 
 
 //event listener for search 
-searchBtn.addEventListener("onClick", formSubmitHandler);
+searchBtn.addEventListener("click", formSubmitHandler);
